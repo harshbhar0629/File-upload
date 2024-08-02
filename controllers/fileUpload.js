33b19d01ef6f7d11eb263d8bbc8b2e2cb21fc499
harshbhar0629 <harshbhar.0629@gmail.com>
2024-08-02 22:58:50 +0530

    File upload post route complete

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -5,7 +5,17 @@ exports.localFileUpload = async (req, res) => {
     try {
         // file fetch 
         const file = req.files.file;
-        const path = __dirname + "/files/" + Date.now();
+        console.log(file)
+        let path = __dirname + "/files/" + Date.now() + "." + file.name.split('.')[1];
+        console.log(path)
+        file.mv(path, () => {
+            console.log("file save succesfully");
+        });
+
+        res.json({
+            success: true,
+            message: "Local file uploaded successfully"
+        });
         
     } catch (err) {
         console.log("Error in local file uploading");
